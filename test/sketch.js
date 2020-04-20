a201d3016bfd9e9448663616de9cae6aa5a46f93
wind <winbring@gmail.com>
2020-04-20 18:30:31 +0300

    Reorg and deleted duplicate libs

---
+++ test/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(920, 960);
+}
+
+function draw() {
+  noStroke();
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
